6d8a08415982e3b232f9b63de6082aefb1258cd0
konyvkukac <ihasz.rob.gmail.com>
2020-07-26 19:56:16 +0200

    On branch master  Your branch is up to date with 'origin/master'. Changes to be committed: 	new file:   fonts/Aclonica/Aclonica-Regular.ttf 	new file:   fonts/Aclonica/Aclonica.zip 	new file:   fonts/Aclonica/LICENSE.txt 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular.ttf 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/@font-face/Demo.html 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/@font-face/fdcb4e99330f56ecee5486a1233bd0cc.eot 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/@font-face/fdcb4e99330f56ecee5486a1233bd0cc.svg 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/@font-face/fdcb4e99330f56ecee5486a1233bd0cc.ttf 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/@font-face/fdcb4e99330f56ecee5486a1233bd0cc.woff 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/@font-face/fdcb4e99330f56ecee5486a1233bd0cc.woff2 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/CoreSansA45Regular.ttf 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/License.txt 	new file:   fonts/Core Sans 45 Regular/CoreSansA45Regular/Online_Web_Fonts.url 	new file:   fonts/Lemonada/Lemonada-VariableFont_wght.ttf 	new file:   fonts/Lemonada/Lemonada.zip 	new file:   fonts/Lemonada/OFL.txt 	new file:   fonts/Lemonada/README.txt 	new file:   fonts/Lemonada/static/Lemonada-Bold.ttf 	new file:   fonts/Lemonada/static/Lemonada-Light.ttf 	new file:   fonts/Lemonada/static/Lemonada-Medium.ttf 	new file:   fonts/Lemonada/static/Lemonada-Regular.ttf 	new file:   fonts/Lemonada/static/Lemonada-SemiBold.ttf 	new file:   fonts/Lobster/Lobster-Regular.ttf 	new file:   fonts/Lobster/Lobster.zip 	new file:   fonts/Lobster/OFL.txt 	new file:   fonts/OnlineWebFonts_COM_219fcfd4dea998b55d90eb3635dcbf3e.zip 	new file:   fonts/Shadow_Into_Light/OFL.txt 	new file:   fonts/Shadow_Into_Light/ShadowsIntoLight-Regular.ttf 	new file:   fonts/Shadow_Into_Light/Shadows_Into_Light.zip 	new file:   fonts/Yellowtail/LICENSE.txt 	new file:   fonts/Yellowtail/Yellowtail-Regular.ttf 	new file:   fonts/Yellowtail/Yellowtail.zip 	modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-function validateForm() {  
-    // Termék egységára
-           let pricePerPiece = 1200;   
+
+    function szamitas() {
+        let szam1 = document.querySelector("input[name='szam1']"); //szam1 változó létrehozása, értéke a name='szam1' értéke lesz
+        let szam2 = document.querySelector("input[name='szam2']"); //szám2 változó létrehozás, kiolvasása  
+        let total = parseInt(szam1.value) + parseInt(szam2.value);   //total változó létrehozása, művelet elvégzése a két előző változó értékével
+        
+        let Szamla = document.querySelector("span.Szamla")  //változó létrehozása a Szamla összegének kiíratására
+        Szamla.innerHTML = total;     //az .innerHTML módosítja a 'Szamla' értékét a 'total' aktuális értékével 
+    }
     
-                // Kiválasztom a mennyiséget és a message mező ár részét.  
-        let totalinput = document.querySelector("form#order input[name=total-input]");
-        let priceField = document.querySelector("form#order .message strong");   
+    function szorzas() {
+        var pizza = (1200);
+        let darab = document.querySelector("input[name='darab']");
+        let osszeg = pizza * parseInt(darab.value);
     
-                // Kiolvasom a bevitt mennyiséget és szorzom az egységárral.  
-        let total = parseInt(totalinput.value);  
-        let totalPrice  = total * pricePerPiece;   
-    alert (totalPrice);
+        //Beír adat ellenőrzése
+         if (darab.value > 10) {                
+             alert("Maximum 10 terméket vásárolhat!");
+             return;
+         }
+         else if (darab.value < 1) {
+                 alert("Kérem, legalább egy terméket jelöljön meg!");
+                 return ;
+                }
+         
+        let szorzat = document.querySelector("span.szorzat") //Fizetendő összeg értékének lekérése, változóba mentése.
+        szorzat.innerHTML = osszeg;  //A HTML kód 'szorzat span' mező értékének kiíratása. 
+        
+    }
+    /*function myFunction() {
+        var x = document.getElementById("demo");
+        x.style.fontSize = "25px";
+        x.style.color = "red";} */
+      
